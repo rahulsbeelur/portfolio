edcce3787df1b1e4f9f79c1111922bd2ab8ba01f
rahulsbeelur <srahulbeelur@gmail.com>
2023-07-17 18:40:33 +0530

    Tablet appbar delay for 300ms

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import { Wrapper } from '../modules/common/components/Wrapper';
 const Home = (): JSX.Element => {
     return (
         <Wrapper>
-            <div>HOME Page</div>
+            <div>
+                HOME Page Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio totam
+                expedita magnam voluptates tempora! Aspernatur sequi debitis excepturi iure deleniti
+                nihil nobis voluptas quaerat fugit eaque! Quo assumenda ipsam dolore!
+            </div>
         </Wrapper>
     );
 };
