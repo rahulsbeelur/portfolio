0189664459776ec926adb8cdb93a6631efa190c6
rahulsbeelur <srahulbeelur@gmail.com>
2023-07-23 00:44:26 +0530

    Personal photo change

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,16 +23,16 @@ const Home = ({
                     <div className="pl-[22%] tablet:pl-[15%] py-[5%] mobile:p-0 mobile:justify-center flex flex-row mobile:flex-col h-full gap-10">
                         <div className="w-[40%] mobile:w-[100%] h-[50%] shadow-md shadow-neutral-black-default mobile:mt-[120px]">
                             <div className="flex flex-col gap-6 bg-primary-background-color-lighter dark:bg-neutral-black-lighter text-neutral-black-darker p-10 text-center">
-                                <div className="rounded-full overflow-hidden w-[80%] ml-auto mr-auto">
+                                <div className="rounded-full overflow-hidden w-[250px] h-[250px] tablet:w-[150px] tablet:h-[150px] mobile:w-[225px] mobile:h-[225px] ml-auto mr-auto">
                                     <Image
-                                        src="/personal-photo.png"
+                                        src="/my-photo.jpeg"
                                         alt="personal image"
                                         width={1000}
                                         height={1000}
-                                        className="object-cover w-full h-full"
+                                        className="object-cover w-full"
                                     />
                                 </div>
-                                <p className="desktop:h5 tablet:h5 h5 font-[700] mt-auto mb-auto text-neutral-black-dark ">
+                                <p className="desktop:h5 tablet:h5 h5 font-[700] mt-auto mb-auto text-neutral-black-dark">
                                     {personDetails.name}
                                 </p>
                                 <div className="border border-1 text-neutral-black-dark w-[20%] ml-auto mr-auto"></div>
