51ec8edfbca5a0e329cbbd81c0f2c2360617d8a8
rahulsbeelur <srahulbeelur@gmail.com>
2023-07-24 23:12:57 +0530

    Add MySQL

--- constant/paths.ts
+++ constant/paths.ts
@@ -8,9 +8,9 @@ export enum MdxPaths {
 }
 
 export enum TechnologiesPath {
+    ProgrammingLanguages = 'modules/tech-stack/programming-languages/technologies.yml',
+    FrontEnd = 'modules/tech-stack/front-end/technologies.yml',
     BackEnd = 'modules/tech-stack/back-end/technologies.yml',
     DevOps = 'modules/tech-stack/dev-ops/technologies.yml',
-    FrontEnd = 'modules/tech-stack/front-end/technologies.yml',
-    ProgrammingLanguages = 'modules/tech-stack/programming-languages/technologies.yml',
     VersionControl = 'modules/tech-stack/version-control/technologies.yml'
 }
