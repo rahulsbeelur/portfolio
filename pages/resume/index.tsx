80dc51c79372003cbc3192e0b13ef66d9cb74121
rahulsbeelur <srahulbeelur@gmail.com>
2023-12-12 23:38:24 +0530

    Resume page layout

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -4,11 +4,11 @@ import { Download } from '../../icons/Download';
 
 const Resume = (): JSX.Element => {
     return (
-        <Wrapper>
-            <div className="mx-auto max-w-[2560px] desktop:mt-[138px] tablet:mt-[128px] mt-20">
-                <div className="flex justify-end mobile:justify-center">
+        <Wrapper classes="bg-primary-background-color dark:bg-neutral-black-light">
+            <div className="mx-auto max-w-[2560px] desktop:mt-[138px] tablet:mt-[128px] mt-24">
+                <div className="flex justify-center mobile:justify-center">
                     <button
-                        className="h5 mobile:small1 tablet:h6 bg-primary-background-color hover:bg-primary-background-color-darker dark:bg-neutral-black-light hover:dark:bg-neutral-black-default p-3 rounded-lg shadow-[0_4px_8px_rgba(28,28,40)] dark:shadow-lg dark:shadow-[#50504e] text-neutral-black-darker"
+                        className="h5 mobile:sub-headline3 tablet:sub-headline2 bg-white dark:bg-neutral-black-darker hover:bg-primary-background-color-darker hover:dark:bg-neutral-black-default p-3 rounded-lg shadow-[0_4px_8px_rgba(28,28,40)] dark:shadow-[0_4px_12px_rgba(80,80,78)] text-neutral-black-darker dark:text-white"
                         aria-label="Download Resume"
                         onClick={(): void => document.getElementById('resumeToDownload')?.click()}>
                         Download Resume
@@ -23,6 +23,22 @@ const Resume = (): JSX.Element => {
                             id="resumeToDownload"
                         />
                     </button>
+                </div>
+                <div className="flex justify-center mt-10 text-neutral-black-darker">
+                    <div className="w-[60%] tablet:w-[80%] mobile:w-full">
+                        <div className="desktop:sub-headline1 sub-headline2 ">Work Experience</div>
+                        <div className="desktop:sub-headline3 sub-headline4 bg-white dark:bg-neutral-black-darker mt-5 p-3 rounded-lg shadow-[0_4px_8px_rgba(28,28,40)] dark:shadow-[0_4px_12px_rgba(80,80,78)] dark:text-white">
+                            Card
+                        </div>
+                        <div className="desktop:sub-headline1 sub-headline2 mt-20">Education</div>
+                        <div className="desktop:sub-headline3 sub-headline4 bg-white dark:bg-neutral-black-darker mt-5 p-3 rounded-lg shadow-[0_4px_8px_rgba(28,28,40)] dark:shadow-[0_4px_12px_rgba(80,80,78)] dark:text-white">
+                            Card
+                        </div>
+                        <div className="desktop:sub-headline1 sub-headline2 mt-20">Languages</div>
+                        <div className="desktop:sub-headline3 sub-headline4 bg-white dark:bg-neutral-black-darker mt-5 p-3 rounded-lg shadow-[0_4px_8px_rgba(28,28,40)] dark:shadow-[0_4px_12px_rgba(80,80,78)] dark:text-white">
+                            Card
+                        </div>
+                    </div>
                 </div>
             </div>
         </Wrapper>
