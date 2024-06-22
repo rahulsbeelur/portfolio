42448adc12fc6db1588cf8fbc2b42c969648e05d
rahulsbeelur <srahulbeelur@gmail.com>
2024-06-22 17:44:48 +0530

    Added google console verification html tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ export const Document = (): JSX.Element => {
     return (
         <Html className="scroll-smooth">
             <Head>
+                <meta name="google-site-verification" content="v-29Y0BgPVzesTZmVjb8oXuAU5h7fC3bKzfGoDsX6wk" />
                 <meta name="color-scheme" content="dark light"></meta>
                 <meta name="msapplication-TileColor" content="#da532c" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
