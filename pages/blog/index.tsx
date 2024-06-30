0a0c5d04d3df40cf30d328c187fe53698f7e9526
rahulsbeelur <srahulbeelur@gmail.com>
2024-06-30 15:27:32 +0530

    Blog page fix

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,9 +3,9 @@ import { Wrapper } from '../../modules/common/components/Wrapper';
 const BlogPage = (): JSX.Element => {
     return (
         <Wrapper classes="bg-primary-background-color dark:bg-neutral-black-light normal-case">
-            <div className="mt-[100px] h-[400px] flex flex-col justify-center align-middle h3 text-center mobile:h5 gap-4">
-                <div>404: Blog not found... Just kidding!</div>
-                <div className="h5 mobile:sub-headline2">Compiling thoughts. Stay tuned!</div>
+            <div className="mt-[100px] h-[400px] flex flex-col justify-center align-middle h4 text-center mobile:h5 gap-4">
+                <div>404: Blog not found yet</div>
+                <div className="h6 mobile:sub-headline2">Compiling thoughts. Stay tuned!</div>
             </div>
         </Wrapper>
     );
